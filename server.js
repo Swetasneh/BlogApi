fdccf85907d2892381341579067dcce92d38fd21
Swetasneh <146664899+Swetasneh@users.noreply.github.com>
2023-12-23 18:35:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const blogRoutes = require('./routes/blogRoutes')
 connectDB();
 
 const limiter = rateLimit({
+    validate: {
+		validationsConfig: false,
+		// ...
+		default: true,
+	},
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again later.',
